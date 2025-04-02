db0eca4412e92c867a9e738077ac3d6f29d0d543
Codaix <160418176+Codaix@users.noreply.github.com>
2025-04-02 13:20:12 +0530

    c successful run

---
+++ hi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+cout<<"HI";
+int a;
+cin>>a;
+cout<<a;
+}
