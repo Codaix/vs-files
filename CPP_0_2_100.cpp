602844d1beca85bacd94ec040a389daaa607e40b
Codaix <160418176+Codaix@users.noreply.github.com>
2025-04-18 13:08:27 +0530

    generic (new)

--- CPP_0_2_100.cpp
+++ CPP_0_2_100.cpp
@@ -1,42 +1,29 @@
-// #include <iostream>
-// #include <stdlib.h>
-// #include<stdio.h>
-// #include<math.h>
-// using namespace std;
-
-// int main(){
-//     int *ptr;
-//     int var = 23;
-//     ptr = &var;
-//     // *ptr = var;
-//     // *ptr = &var;
-//     cout<<ptr<<endl;
-//     cout<<*ptr<<endl;
-//     cout<<&var<<endl;
-//     cout<<var<<endl;
-
-// }
-// #include <iostream>
-// #include <iomanip>
-
-// int main() {
-//   double pi = 3.14159265358979323846;
-//   // Set field width and precision for output
-//   std::cout << std::setw(10) << std::setprecision(5) << pi << std::endl; // Output:   3.1416
-//   std::cout << std::setw(15) << std::setfill('0') << 123 << std::endl; // Output: 000000123
-//   std::cout << std::setbase(16) << 15 << std::endl; // Output: f (hexadecimal)
-
-//   return 0;
-// }
 #include <iostream>
 using namespace std;
-int main(){
-  int i = 0;
-  int n = 1;
-  do{
-    cout<<i<<endl;
-    i++;
-    n= n+1;
+
+class Point{
+  int x, y;
+  public:
+
+  Point(int a =0, int b =0){
+    x = a;
+    y = b;
   }
-  while(n<12);
+  Point operator + (Point const& p){
+    Point result;
+    result.x = x + p.x;
+    result.y = y + p.y;
+    return result;
+  }
+  void display(){
+    cout<<"("<<x<<","<<y<<")"<<endl;
+  }
+
+};
+int main(){
+  Point p1(4,6), p2(5,8), p5(6,7);
+  Point p3 = p1+p2+p5;
+  p3.display();
+  return 0;
+
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,2 +1,8 @@
-names.add("Alex");
-        names.add("Bob");
+ArrayList <String>colors = new ArrayList<>();
+        // colors.add("Red");
+        // colors.add("Blue");
+        // colors.add("Green");
+        // System.out.println(colors);        
+        // System.out.println("Frist color: "+colors.get(0));
+        // colors.remove(2);
+        // System.out.println(colors);
