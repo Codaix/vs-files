2971a8730253b38fb7406c7dd28ad54e5aeca9d5
Codaix <160418176+Codaix@users.noreply.github.com>
2025-05-21 22:23:09 +0530

    Array List Changes

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,8 +1,8 @@
 ArrayList <String>colors = new ArrayList<>();
-        // colors.add("Red");
-        // colors.add("Blue");
-        // colors.add("Green");
-        // System.out.println(colors);        
-        // System.out.println("Frist color: "+colors.get(0));
-        // colors.remove(2);
-        // System.out.println(colors);
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        System.out.println(colors);        
+        System.out.println("Frist color: "+colors.get(0));
+        colors.remove(2);
+        System.out.println(colors);
